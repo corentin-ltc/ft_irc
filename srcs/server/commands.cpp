b2897879dc06853accb9efe5de53ec28f6c3bacc
David Li <davli@bess-f4r1s7.clusters.42paris.fr>
2025-01-15 16:59:10 +0100

    Clean handlers

--- srcs/server/commands.cpp
+++ srcs/server/commands.cpp
@@ -10,3 +10,29 @@ void Server::handleCommand(Client *client)
 {
 
 }
+
+void Server::joinChannel(Client client, std::string input)
+{
+	std::string channel = input.substr(5, input.size());
+	if (channel.size() == 0)
+		std::cout << client.getSocket() << "Usage: /join <channel_name>" << std::endl;
+	std::cout << channel << std::endl;
+	std::vector<Channel>::iterator it;
+	for (it = channels.begin(); it != channels.end(); it++)
+	{
+		if (it[0].getName() == channel)
+		{
+			std::cout << "Client " << client.getSocket() << " has join the existing channel : " << channel << std::endl;
+			it[0].addUser(client);
+			break;
+		}
+	}
+    if (it == channels.end())
+	{
+		Channel newChan(channel);
+		channels.push_back(newChan);
+		std::cout << "Client " << client.getSocket() << " successfully created the channel : " << channel << std::endl;
+		newChan.addUser(client);
+
+	}
+}
