be17f6a5d19fa97cc97d0837ea7265d7bc228e14
Nabil080 <bellilanabil8@gmail.com>
2025-01-16 10:05:50 +0100

    Port and password parameters

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -1,25 +1,27 @@
 #pragma once
 
-#include "ft_irc.hpp"
 #include "Client.hpp"
 #include "Server.hpp"
+#include "ft_irc.hpp"
 
 class Channel
 {
-    private:
-        //unsigned int            max_users;
-        std::string             name;
-        std::string             password;
-        std::vector<Client>     users;
-        std::vector<Client>     operators;
-    public:
-        // Constructors:
-        Channel(std::string name) : name(name){};
+  private:
+	// unsigned int            max_users;
+	std::string name;
+	std::string password;
+	std::vector<Client *> users;
+	std::vector<Client *> operators;
 
-        // Getter and setters:
-            std::string getName(){return name;}
-        // Members functions
-            void    addUser(Client client);
-		
+  public:
+	// Constructors:
+	Channel(std::string name) : name(name) {};
 
+	// Getter and setters:
+	std::string getName()
+	{
+		return name;
+	}
+	// Members functions
+	void addUser(Client client);
 };
