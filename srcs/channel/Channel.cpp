07482da68e0f525c7bd801e6a4555e7358822395
Nabil080 <bellilanabil8@gmail.com>
2025-01-20 16:23:40 +0100

    Added helper functions

--- srcs/channel/Channel.cpp
+++ srcs/channel/Channel.cpp
@@ -2,39 +2,55 @@
 #include "Server.hpp"
 #include "ft_irc.hpp"
 
-
-void    Channel::addUser(Client *client)
+std::string Channel::getName() const
 {
-    users.push_back(client);
-    if (client->isGlobalOperator())
-        operators.push_back(client);
-
-    std::string beginning_of_message = ":" + client->getNickname() + "!" + client->getUsername() + "@localhost ";
-    
-    // First message
-    std::string message = beginning_of_message + "353 " + client->getNickname() + " = " + name + " :" + getUsersInChannel();
-    Server::sendToSocket(client->getSocket(), message);
-
-    //Second message
-    message = beginning_of_message + "366 " + client->getNickname() + " " + name + " :End of /NAMES list";
-    Server::sendToSocket(client->getSocket(), message);
-
-    //Third message
-    message = beginning_of_message + "JOIN :" + name;
-    Server::sendToSocket(client->getSocket(), message);
-
+	return (this->name);
 }
 
-
-std::string    Channel::getUsersInChannel()
+std::string Channel::getPassword() const
 {
-    std::string list= "";
+	return (this->password);
+}
+std::vector<Client *> Channel::getUsers() const
+{
+	return (this->operators);
+}
+std::vector<Client *> Channel::getOperators() const
+{
+	return (this->operators);
+}
 
-    for (std::vector<Client*>::iterator it = users.begin(); it != users.end(); it++)
-    {
-        list.append((*it)->getNickname());
-        if (it + 1 != users.end())
-            list.append(" ");
-    }
-    return list;
+void Channel::addUser(Client *client)
+{
+	users.push_back(client);
+	if (client->isGlobalOperator())
+		operators.push_back(client);
+
+	std::string beginning_of_message = ":" + client->getNickname() + "!" + client->getUsername() + "@localhost ";
+
+	// First message
+	std::string message =
+		beginning_of_message + "353 " + client->getNickname() + " = " + name + " :" + getUsersString();
+	Server::sendToSocket(client->getSocket(), message);
+
+	// Second message
+	message = beginning_of_message + "366 " + client->getNickname() + " " + name + " :End of /NAMES list";
+	Server::sendToSocket(client->getSocket(), message);
+
+	// Third message
+	message = beginning_of_message + "JOIN :" + name;
+	Server::sendToSocket(client->getSocket(), message);
+}
+
+std::string Channel::getUsersString()
+{
+	std::string list = "";
+
+	for (std::vector<Client *>::iterator it = users.begin(); it != users.end(); it++)
+	{
+		list.append((*it)->getNickname());
+		if (it + 1 != users.end())
+			list.append(" ");
+	}
+	return list;
 }
